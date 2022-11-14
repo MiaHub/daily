c36455705e4449a13792422ef64f6ba5e9b7ee83
MiaHub <96913317+MiaHub@users.noreply.github.com>
2022-11-14 20:09:05 +0100

    Minor3

--- admin/index.php
+++ admin/index.php
@@ -1,31 +1,95 @@
-<html>
-    <head>
-         <title>Daily</title>
 
-        <link rel="stylesheet" href="../css/admin.css">     
-    </head>
+<?php include('partials/menu.php'); ?>
 
-    <body>
-        <div class="menu">
-            <div class="wrapper">
-            Menu goes here
-            </div>
-        </div>
-    </body>
-
-    <body>
+        <!-- Main Content Section Starts -->
         <div class="main-content">
             <div class="wrapper">
-            Main Content goes here
-            </div>
-        </div>
-    </body>
+                <h1>Dashboard</h1>
+                <br><br>
+                <?php 
+                    if(isset($_SESSION['login']))
+                    {
+                        echo $_SESSION['login'];
+                        unset($_SESSION['login']);
+                    }
+                ?>
+                <br><br>
 
-    <body>
-        <div class="footer">
-            <div class="wrapper">
-                <p class="text-center"> 2022 All rights reserved, Daily - <a href="#"> Dieter Cho</a></p>
+                <div class="col-4 text-center">
+
+                    <?php 
+                        //Sql Query 
+                        $sql = "SELECT * FROM tbl_category";
+                        //Execute Query
+                        $res = mysqli_query($conn, $sql);
+                        //Count Rows
+                        $count = mysqli_num_rows($res);
+                    ?>
+
+                    <h1><?php echo $count; ?></h1>
+                    <br />
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+
+                    <?php 
+                        //Sql Query 
+                        $sql2 = "SELECT * FROM tbl_food";
+                        //Execute Query
+                        $res2 = mysqli_query($conn, $sql2);
+                        //Count Rows
+                        $count2 = mysqli_num_rows($res2);
+                    ?>
+
+                    <h1><?php echo $count2; ?></h1>
+                    <br />
+                    Foods
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    <?php 
+                        //Sql Query 
+                        $sql3 = "SELECT * FROM tbl_order";
+                        //Execute Query
+                        $res3 = mysqli_query($conn, $sql3);
+                        //Count Rows
+                        $count3 = mysqli_num_rows($res3);
+                    ?>
+
+                    <h1><?php echo $count3; ?></h1>
+                    <br />
+                    Total Orders
+                </div>
+
+                <div class="col-4 text-center">
+                    
+                    <?php 
+                        //Creat SQL Query to Get Total Revenue Generated
+                        //Aggregate Function in SQL
+                        $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered'";
+
+                        //Execute the Query
+                        $res4 = mysqli_query($conn, $sql4);
+
+                        //Get the VAlue
+                        $row4 = mysqli_fetch_assoc($res4);
+                        
+                        //GEt the Total REvenue
+                        $total_revenue = $row4['Total'];
+
+                    ?>
+
+                    <h1>$<?php echo $total_revenue; ?></h1>
+                    <br />
+                    Revenue Generated
+                </div>
+
+                <div class="clearfix"></div>
+
             </div>
         </div>
-    </body>
-</html>
+        <!-- Main Content Setion Ends -->
+
+<?php include('partials/footer.php') ?>
