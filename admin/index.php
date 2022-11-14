b10c730c0e8bb86407d7e04aea0a4edbf9b6c4ac
MiaHub <96913317+MiaHub@users.noreply.github.com>
2022-11-14 19:06:33 +0100

    Minor1

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,31 @@
 <html>
     <head>
          <title>Daily</title>
+
+        <link rel="stylesheet" href="../css/admin.css">     
     </head>
 
     <body>
-        <h1>Admin Panel</h1>
+        <div class="menu">
+            <div class="wrapper">
+            Menu goes here
+            </div>
+        </div>
+    </body>
+
+    <body>
+        <div class="main-content">
+            <div class="wrapper">
+            Main Content goes here
+            </div>
+        </div>
+    </body>
+
+    <body>
+        <div class="footer">
+            <div class="wrapper">
+            Footer goes here
+            </div>
+        </div>
     </body>
 </html>
